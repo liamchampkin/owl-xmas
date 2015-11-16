30374d915ac102f57f3cc18c18c2eaaf583a166c
Liam Smith <liam.smith@oup.com>
2015-11-16 10:05:31 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function() {
+  var config = {
+    kitId: 'jgr8wsf'
+  };
+  var d = false;
+  var tk = document.createElement('script');
+  tk.src = '//use.typekit.net/' + config.kitId + '.js';
+  tk.type = 'text/javascript';
+  tk.async = 'true';
+  tk.onload = tk.onreadystatechange = function() {
+    var rs = this.readyState;
+    if (d || rs && rs != 'complete' && rs != 'loaded') return;
+    d = true;
+    try { Typekit.load(config); } catch (e) {}
+  };
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(tk, s);
+})();
