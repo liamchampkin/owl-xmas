b509854ca531962394f2cdbc72ce01ea969011f1
Liam Smith <liam.smith@oup.com>
2015-11-17 17:10:55 +0000

    finished artwork added tab 2

--- js/script.js
+++ js/script.js
@@ -16,3 +16,60 @@
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(tk, s);
 })();
+
+//amimation
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    (function($) {
+      console.log('animate-in');
+      /**
+       * Copyright 2012, Digital Fusion
+       * Licensed under the MIT license.
+       * http://teamdf.com/jquery-plugins/license/
+       *
+       * @author Sam Sehnert
+       * @desc A small plugin that checks whether elements are within
+       *     the user visible viewport of a web browser.
+       *     only accounts for vertical position, not horizontal.
+       */
+
+      $.fn.visible = function(partial) {
+        
+          var $t            = $(this),
+              $w            = $(window),
+              viewTop       = $w.scrollTop(),
+              viewBottom    = viewTop + $w.height(),
+              _top          = $t.offset().top,
+              _bottom       = _top + $t.height(),
+              compareTop    = partial === true ? _bottom : _top,
+              compareBottom = partial === true ? _top : _bottom;
+        
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+      };
+        
+    })(jQuery);
+
+    var win = $(window);
+
+    var allMods = $(".section-book");
+
+    allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("already-visible"); 
+      } 
+    });
+
+    win.scroll(function(event) {
+      
+      allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+          el.addClass("animate-in"); 
+        } 
+      });
+      
+    });
+});
