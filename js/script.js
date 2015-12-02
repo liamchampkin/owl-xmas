7f9d80cbb376df622560321cf30097e6498941fe
Liam Smith <liam.smith@oup.com>
2015-12-02 09:39:16 +0000

    fixed a few safari bugs

--- js/script.js
+++ js/script.js
@@ -72,4 +72,32 @@ $( document ).ready(function() {
       });
       
     });
+
+ var gotoHashTab = function (customHash) {
+        var hash = customHash || location.hash;
+        var hashPieces = hash.split('?'),
+            activeTab = $('[href=' + hashPieces[0] + ']');
+        activeTab && activeTab.tab('show');
+    }
+
+    // onready go to the tab requested in the page hash
+    gotoHashTab();
+
+return $('a[data-toggle="tab"]').on('shown', function(e) {
+location.hash = $(e.target).attr('href').substr(1);
+scrollTo(0,0);
 });
+
+    // when the nav item is selected update the page hash
+    $('.nav a').on('shown', function (e) {
+        window.location.hash = e.target.hash;
+    })
+
+    // when a link within a tab is clicked, go to the tab requested
+    $('.tab-pane a').click(function (event) {
+        if (event.target.hash) {
+            gotoHashTab(event.target.hash);
+        }
+    });
+});
+
